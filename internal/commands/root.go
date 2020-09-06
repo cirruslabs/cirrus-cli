5a14aa94168f693512286dde4ff0d3a09867d532
Nikolay Edigaryev <edigaryev@gmail.com>
2020-09-06 18:56:14 +0300

    Support --version flag
    Resolves #56.

--- internal/commands/root.go
+++ internal/commands/root.go
@@ -2,10 +2,13 @@ package commands
 
 import "github.com/spf13/cobra"
 
+var version string
+
 func NewRootCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "cirrus",
-		Short: "Cirrus CLI",
+		Use:     "cirrus",
+		Short:   "Cirrus CLI",
+		Version: version,
 	}
 
 	cmd.AddCommand(
