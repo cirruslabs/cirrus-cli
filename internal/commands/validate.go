b00a93c1bab992afd63f6469e24f13afc10828be
Nikolay Edigaryev <edigaryev@gmail.com>
2020-09-08 19:21:53 +0300

    Introduce hidden "--json" flag for validate command

--- internal/commands/validate.go
+++ internal/commands/validate.go
@@ -2,6 +2,8 @@ package commands
 
 import (
 	"errors"
+	"fmt"
+	"github.com/cirruslabs/cirrus-cli/internal/testutil"
 	"github.com/cirruslabs/cirrus-cli/pkg/rpcparser"
 	"github.com/spf13/cobra"
 	"log"
@@ -10,6 +12,7 @@ import (
 var ErrValidate = errors.New("validate failed")
 
 var validateFile string
+var yaml bool
 
 func validate(cmd *cobra.Command, args []string) error {
 	// https://github.com/spf13/cobra/issues/340#issuecomment-374617413
@@ -29,6 +32,10 @@ func validate(cmd *cobra.Command, args []string) error {
 		return ErrValidate
 	}
 
+	if yaml {
+		fmt.Println(string(testutil.TasksToJSON(nil, result.Tasks)))
+	}
+
 	return nil
 }
 
@@ -40,6 +47,13 @@ func newValidateCmd() *cobra.Command {
 	}
 
 	cmd.PersistentFlags().StringVarP(&validateFile, "file", "f", ".cirrus.yml", "use file as the configuration file")
+
+	// A hidden flag to dump YAML representation of tasks and aid in generating test
+	// cases for smooth rpcparser → parser transition
+	cmd.PersistentFlags().BoolVar(&yaml, "json", false, "emit a JSON list with tasks contained in the configuration file")
+	if err := cmd.PersistentFlags().MarkHidden("json"); err != nil {
+		panic(err)
+	}
 
 	return cmd
 }
