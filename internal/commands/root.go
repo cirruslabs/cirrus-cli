43b6e9027e2bdca0832eea467597075cfcb86d2f
fkorotkov <fedor.korotkov@gmail.com>
2020-09-10 15:24:45 -0400

    Fall back to module version
    If version is explicitly not configured

--- internal/commands/root.go
+++ internal/commands/root.go
@@ -1,10 +1,20 @@
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"runtime/debug"
+)
 
 var version string
 
 func NewRootCmd() *cobra.Command {
+	if version == "" {
+		info, ok := debug.ReadBuildInfo()
+		if ok && info != nil {
+			version = info.Main.Version
+		}
+	}
+
 	cmd := &cobra.Command{
 		Use:     "cirrus",
 		Short:   "Cirrus CLI",
