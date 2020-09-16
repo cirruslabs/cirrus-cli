432bd9726992d6f3539dbae67c7c4b2dd2d88e29
Nikolay Edigaryev <edigaryev@gmail.com>
2020-09-16 18:52:01 +0300

    Consistent version format (#88)
    * No need to check if info is non-nil if ok is true
    
    * Split version into version and commit and move them to the main package
    
    * Parse the version retrieved from BuildInfo
    
    * Keep the version and commit in the commands package for now
    
    Changing lots of NewRootCmd() signatures in tests not worth it.
    
    * go fmt

--- internal/commands/root.go
+++ internal/commands/root.go
@@ -1,24 +1,37 @@
 package commands
 
 import (
+	"fmt"
+	goversion "github.com/hashicorp/go-version"
 	"github.com/spf13/cobra"
 	"runtime/debug"
 )
 
-var version string
+var (
+	version = "unknown"
+	commit  = "unknown"
+)
 
 func NewRootCmd() *cobra.Command {
-	if version == "" {
+	if version == "unknown" {
 		info, ok := debug.ReadBuildInfo()
-		if ok && info != nil {
-			version = info.Main.Version
+		if ok {
+			// We parse the version here for two reasons:
+			// * to weed out the "(devel)" version and fallback to "unknown" instead
+			//   (see https://github.com/golang/go/issues/29228 for details on when this might happen)
+			// * to remove the "v" prefix from the BuildInfo's version (e.g. "v0.7.0") and thus be consistent
+			//   with the binary builds, where the version string would be "0.7.0" instead
+			semver, err := goversion.NewSemver(info.Main.Version)
+			if err == nil {
+				version = semver.String()
+			}
 		}
 	}
 
 	cmd := &cobra.Command{
 		Use:     "cirrus",
 		Short:   "Cirrus CLI",
-		Version: version,
+		Version: fmt.Sprintf("%s-%s", version, commit),
 	}
 
 	cmd.AddCommand(
