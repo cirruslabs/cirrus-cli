596b99ea99f8257e204b48ce9ff6471e816624ab
fkorotkov <fedor.korotkov@gmail.com>
2020-09-21 16:56:27 -0400

    Check for EADDRNOTAVAIL

--- internal/executor/rpc/rpc.go
+++ internal/executor/rpc/rpc.go
@@ -22,6 +22,7 @@ import (
 	"runtime"
 	"strings"
 	"sync"
+	"syscall"
 
 	// Registers a gzip compressor needed for streaming logs from the agent.
 	_ "google.golang.org/grpc/encoding/gzip"
@@ -140,7 +141,7 @@ func (r *RPC) Start(ctx context.Context) error {
 
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
-		if strings.Contains(err.Error(), "bind: cannot assign requested address") {
+		if errors.Is(err, syscall.EADDRNOTAVAIL) {
 			return fmt.Errorf("%w: failed to assign Docker network bridge address %s (is Docker running?)", ErrRPCFailed, address)
 		}
 		return fmt.Errorf("%w: failed to start RPC service on %s: %v", ErrRPCFailed, address, err)
