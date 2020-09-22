9c15e5bb22d7586ddd61b825b1e63577d1912d60
Nikolay Edigaryev <edigaryev@gmail.com>
2020-09-22 21:33:43 +0300

    Task filtering by labels (#101)
    * Task filtering by labels
    
    Resolves #63.
    
    * Wrap label count comparison into containsAll() utility function
    
    * Merge matchLabels() into containsAll()
    
    * Rename soughtLabel to desiredLabel for consistency
    
    * Swap containsAll() arguments

--- internal/executor/taskfilter/taskfilter.go
+++ internal/executor/taskfilter/taskfilter.go
@@ -18,14 +18,62 @@ func MatchExactTask(desiredTaskName string) TaskFilter {
 		var filteredTasks []*api.Task
 
 		for _, task := range tasks {
-			if strings.EqualFold(desiredTaskName, task.Name) {
-				// Clear task's dependencies
-				task.RequiredGroups = task.RequiredGroups[:0]
+			// Ensure that this task's name matches with the name we're looking for
+			desiredTaskNameLower := strings.ToLower(desiredTaskName)
+			taskNameLower := strings.ToLower(task.Name)
 
-				filteredTasks = append(filteredTasks, task)
+			if !strings.HasPrefix(desiredTaskNameLower, taskNameLower) {
+				continue
 			}
+
+			// In case we're looking for a task with specific labels — extract them and ensure they all match
+			desiredLabels := extractLabels(strings.TrimPrefix(desiredTaskNameLower, taskNameLower))
+
+			var actualLabels []string
+			if task.Metadata != nil {
+				actualLabels = task.Metadata.UniqueLabels
+			}
+
+			if !containsAll(actualLabels, desiredLabels) {
+				continue
+			}
+
+			// Clear task's dependencies
+			task.RequiredGroups = task.RequiredGroups[:0]
+
+			filteredTasks = append(filteredTasks, task)
 		}
 
 		return filteredTasks
 	}
+}
+
+func extractLabels(s string) (result []string) {
+	labels := strings.Split(s, " ")
+
+	// Filter out empty labels
+	for _, label := range labels {
+		if strings.TrimSpace(label) == "" {
+			continue
+		}
+
+		result = append(result, label)
+	}
+
+	return
+}
+
+func containsAll(actualLabels []string, desiredLabels []string) bool {
+	var numMatchedLabels int
+
+	for _, desiredLabel := range desiredLabels {
+		for _, actualLabel := range actualLabels {
+			if strings.EqualFold(desiredLabel, actualLabel) {
+				numMatchedLabels++
+				break
+			}
+		}
+	}
+
+	return numMatchedLabels == len(desiredLabels)
 }
