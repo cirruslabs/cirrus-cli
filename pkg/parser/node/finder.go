461d3fb3cec5c4426982f848260ce74efb481300
Nikolay Edigaryev <edigaryev@gmail.com>
2020-09-29 16:01:41 +0500

    DeepFindChild: iterate over child nodes in reverse order

--- pkg/parser/node/finder.go
+++ pkg/parser/node/finder.go
@@ -5,7 +5,9 @@ func (node *Node) DeepFindChild(name string) *Node {
 	var virtualNode Node
 
 	for current := node; current != nil; current = current.Parent {
-		for _, child := range current.Children {
+		for i := len(current.Children) - 1; i >= 0; i-- {
+			child := current.Children[i]
+
 			if child.Name != name {
 				continue
 			}
@@ -15,7 +17,9 @@ func (node *Node) DeepFindChild(name string) *Node {
 				fulfilledAtLeastOnce = true
 			}
 
-			for _, subChild := range child.Children {
+			for i := len(child.Children) - 1; i >= 0; i-- {
+				subChild := child.Children[i]
+
 				// Append fields from child that we don't have
 				if !virtualNode.HasChild(subChild.Name) {
 					virtualNode.Children = append(virtualNode.Children, subChild)
