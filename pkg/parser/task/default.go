37441f725b462203c041b655dadef8126acfb919
fkorotkov <fedor.korotkov@gmail.com>
2020-10-28 09:43:37 -0400

    No executionLock default

--- pkg/parser/task/default.go
+++ pkg/parser/task/default.go
@@ -3,7 +3,6 @@ package task
 func DefaultTaskProperties() map[string]string {
 	return map[string]string{
 		"allowFailures":               "false",
-		"executionLock":               "null",
 		"experimentalFeaturesEnabled": "false",
 		"timeoutInSeconds":            "3600",
 		"triggerType":                 "AUTOMATIC",
