bc001a6e6026a70124fc335c5e0613da1881e24c
fkorotkov <fedor.korotkov@gmail.com>
2020-10-28 09:46:20 -0400

    Labels for proto instances

--- pkg/parser/labels.go
+++ pkg/parser/labels.go
@@ -4,14 +4,21 @@ import (
 	"errors"
 	"fmt"
 	"github.com/cirruslabs/cirrus-ci-agent/api"
+	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/any"
+	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/reflect/protoregistry"
+	"google.golang.org/protobuf/types/dynamicpb"
 	"sort"
+	"strings"
 )
 
-func labels(task *api.Task) ([]string, error) {
-	labels, err := instanceLabels(task.Instance)
+func labels(
+	task *api.Task,
+	additionalInstances map[string]protoreflect.MessageDescriptor,
+) ([]string, error) {
+	labels, err := instanceLabels(task.Instance, additionalInstances)
 	if err != nil {
 		return labels, err
 	}
@@ -24,7 +31,15 @@ func labels(task *api.Task) ([]string, error) {
 	return labels, nil
 }
 
-func instanceLabels(taskInstance *any.Any) ([]string, error) {
+func instanceLabels(
+	taskInstance *any.Any,
+	additionalInstances map[string]protoreflect.MessageDescriptor,
+) ([]string, error) {
+	for instanceName, descriptor := range additionalInstances {
+		if strings.HasSuffix(taskInstance.GetTypeUrl(), string(descriptor.FullName())) {
+			return extractProtoInstanceLabels(taskInstance, instanceName, descriptor)
+		}
+	}
 	// Instance-specific labels
 	var labels []string
 
@@ -66,7 +81,51 @@ func instanceLabels(taskInstance *any.Any) ([]string, error) {
 	return labels, nil
 }
 
-func uniqueLabels(task *api.Task, tasks []*api.Task) ([]string, error) {
+func extractProtoInstanceLabels(anyInstance *any.Any, instanceName string, descriptor protoreflect.MessageDescriptor) ([]string, error) {
+	dynamicInstance := dynamicpb.NewMessage(descriptor)
+
+	var instanceLabels []string
+
+	err := proto.Unmarshal(anyInstance.Value, dynamicInstance)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if fieldDescriptor := descriptor.Fields().ByName("image"); fieldDescriptor != nil {
+		instanceLabels = append(instanceLabels, fmt.Sprintf("%s:%s", instanceName, dynamicInstance.Get(fieldDescriptor)))
+	} else if fieldDescriptor := descriptor.Fields().ByName("image_name"); fieldDescriptor != nil {
+		instanceLabels = append(instanceLabels, fmt.Sprintf("%s:%s", instanceName, dynamicInstance.Get(fieldDescriptor)))
+	} else if fieldDescriptor := descriptor.Fields().ByName("image_id"); fieldDescriptor != nil {
+		instanceLabels = append(instanceLabels, fmt.Sprintf("%s:%s", instanceName, dynamicInstance.Get(fieldDescriptor)))
+	} else if fieldDescriptor := descriptor.Fields().ByName("template"); fieldDescriptor != nil {
+		instanceLabels = append(instanceLabels, fmt.Sprintf("%s:%s", instanceName, dynamicInstance.Get(fieldDescriptor)))
+	}
+
+	if fieldDescriptor := descriptor.Fields().ByName("dockerfilePath"); fieldDescriptor != nil {
+		path := dynamicInstance.Get(fieldDescriptor).String()
+		if path != "" {
+			instanceLabels = append(instanceLabels, fmt.Sprintf("Dockerfile:%s", path))
+		}
+	}
+
+	if fieldDescriptor := descriptor.Fields().ByName("dockerArguments"); fieldDescriptor != nil {
+		dynamicInstance.Get(fieldDescriptor).Map().Range(func(key protoreflect.MapKey, value protoreflect.Value) bool {
+			instanceLabels = append(instanceLabels, fmt.Sprintf("%s:%s", key, value))
+			return true
+		})
+	}
+
+	if fieldDescriptor := descriptor.Fields().ByName("zone"); fieldDescriptor != nil {
+		zone := dynamicInstance.Get(fieldDescriptor).String()
+		if zone != "" {
+			instanceLabels = append(instanceLabels, fmt.Sprintf("zone:%s", zone))
+		}
+	}
+	return instanceLabels, nil
+}
+
+func uniqueLabels(task *api.Task, tasks []*api.Task, additionalInstances map[string]protoreflect.MessageDescriptor) ([]string, error) {
 	// Collect similarly named tasks, including the task itself
 	var similarlyNamedTasks []*api.Task
 
@@ -85,7 +144,7 @@ func uniqueLabels(task *api.Task, tasks []*api.Task) ([]string, error) {
 	commonLabels := make(map[string]int)
 
 	for _, similarlyNamedTask := range similarlyNamedTasks {
-		labels, err := labels(similarlyNamedTask)
+		labels, err := labels(similarlyNamedTask, additionalInstances)
 		if err != nil {
 			return nil, err
 		}
@@ -102,7 +161,7 @@ func uniqueLabels(task *api.Task, tasks []*api.Task) ([]string, error) {
 	}
 
 	// Get labels specific for this task
-	labels, err := labels(task)
+	labels, err := labels(task, additionalInstances)
 	if err != nil {
 		return nil, err
 	}
