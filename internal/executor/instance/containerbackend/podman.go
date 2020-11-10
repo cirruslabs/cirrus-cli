aeeef49ab58af26c7a0d1f199c6bcd11cf6b312f
Nikolay Edigaryev <edigaryev@gmail.com>
2020-11-10 18:39:03 +0300

    Fix tests on non-Linux platforms (#156)
    Some test fail due to Podman not being fully implemented.

--- internal/executor/instance/containerbackend/podman.go
+++ internal/executor/instance/containerbackend/podman.go
@@ -4,6 +4,10 @@ package containerbackend
 
 import "fmt"
 
+type Podman struct {
+	Unimplemented
+}
+
 func NewPodman() (ContainerBackend, error) {
 	return nil, fmt.Errorf("%w: Podman is only supported on Linux", ErrNewFailed)
 }
---
+++ internal/executor/instance/containerbackend/unimplemented.go
@@ -0,0 +1,66 @@
+package containerbackend
+
+import (
+	"context"
+	"io"
+)
+
+type Unimplemented struct{}
+
+func (*Unimplemented) Close() error { return nil }
+
+func (*Unimplemented) ImagePull(ctx context.Context, reference string) error {
+	return ErrNotImplemented
+}
+
+func (*Unimplemented) ImageBuild(
+	ctx context.Context,
+	tarball io.Reader,
+	input *ImageBuildInput,
+) (<-chan string, <-chan error) {
+	logChan := make(chan string)
+	errChan := make(chan error)
+
+	go func() {
+		errChan <- ErrNotImplemented
+	}()
+
+	return logChan, errChan
+}
+
+func (*Unimplemented) ImageInspect(ctx context.Context, reference string) error {
+	return ErrNotImplemented
+}
+
+func (*Unimplemented) VolumeCreate(ctx context.Context, name string) error { return ErrNotImplemented }
+
+func (*Unimplemented) VolumeInspect(ctx context.Context, name string) error { return ErrNotImplemented }
+
+func (*Unimplemented) VolumeDelete(ctx context.Context, name string) error { return ErrNotImplemented }
+
+func (*Unimplemented) ContainerCreate(
+	ctx context.Context,
+	input *ContainerCreateInput,
+	name string,
+) (*ContainerCreateOutput, error) {
+	return nil, ErrNotImplemented
+}
+
+func (*Unimplemented) ContainerStart(ctx context.Context, id string) error { return ErrNotImplemented }
+
+func (*Unimplemented) ContainerWait(ctx context.Context, id string) (<-chan ContainerWaitResult, <-chan error) {
+	waitChan := make(chan ContainerWaitResult)
+	errChan := make(chan error)
+
+	go func() {
+		errChan <- ErrNotImplemented
+	}()
+
+	return waitChan, errChan
+}
+
+func (*Unimplemented) ContainerDelete(ctx context.Context, id string) error { return ErrNotImplemented }
+
+func (*Unimplemented) SystemInfo(ctx context.Context) (*SystemInfo, error) {
+	return nil, ErrNotImplemented
+}
