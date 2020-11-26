bf5a0870008f4b6e4e284117d747f68ba9ce4bf7
Nikolay Edigaryev <edigaryev@gmail.com>
2020-11-26 19:21:18 +0300

    $ go fmt

--- pkg/parser/task/default.go
+++ pkg/parser/task/default.go
@@ -2,9 +2,9 @@ package task
 
 func DefaultTaskProperties() map[string]string {
 	return map[string]string{
-		"allow_failures":     "false",
-		"experimental":       "false",
-		"timeout_in":         "3600",
-		"trigger_type":       "AUTOMATIC",
+		"allow_failures": "false",
+		"experimental":   "false",
+		"timeout_in":     "3600",
+		"trigger_type":   "AUTOMATIC",
 	}
 }
