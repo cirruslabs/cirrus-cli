a3c0421c5239ed00c0b113fc5ad5ffcd15d3632d
Nikolay Edigaryev <edigaryev@gmail.com>
2021-01-25 13:08:49 +0300

    Executor: properly extract hash from the Dockerfile image name

---
+++ internal/executor/executor_internal_test.go
@@ -0,0 +1,39 @@
+package executor
+
+import (
+	"github.com/cirruslabs/cirrus-ci-agent/api"
+	"github.com/cirruslabs/cirrus-cli/internal/executor/instance"
+	"github.com/cirruslabs/cirrus-cli/internal/executor/options"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDockerfileImageTemplate(t *testing.T) {
+	anyInstance, err := ptypes.MarshalAny(&api.PrebuiltImageInstance{
+		Repository: "cirrus-ci-community/d41d8cd98f00b204e9800998ecf8427e",
+		Reference:  "latest",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tasks := []*api.Task{
+		{
+			Name:     "TestDockerfileImageTemplate",
+			Instance: anyInstance,
+		},
+	}
+
+	containerOpts := options.ContainerOptions{
+		DockerfileImageTemplate: "gcr.io/cirrus-ci-community/%s:latest",
+	}
+
+	e, err := New(".", tasks, WithContainerOptions(containerOpts))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "gcr.io/cirrus-ci-community/d41d8cd98f00b204e9800998ecf8427e:latest",
+		e.build.GetTask(0).Instance.(*instance.PrebuiltInstance).Image)
+}
