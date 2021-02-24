d9a064e26da2867cb40f6b4e5a6e0ec53e094c66
Nikolay Edigaryev <edigaryev@gmail.com>
2021-02-25 00:53:26 +0300

    Boolevator: only support newlines in the expression scope (#314)
    ...and not in string scope as it was before.

--- pkg/parser/boolevator/boolevator.go
+++ pkg/parser/boolevator/boolevator.go
@@ -33,26 +33,12 @@ func New(opts ...Option) *Boolevator {
 }
 
 func parseString(ctx context.Context, parser *gval.Parser) (gval.Evaluable, error) {
-	// Work around text/scanner stopping at newline when scanning strings
-	unescaped := strings.ReplaceAll(parser.TokenText(), "\\n", "\n")
-
-	unquoted := strings.Trim(unescaped, "'\"")
+	unquoted := strings.Trim(parser.TokenText(), "'\"")
 
 	return parser.Const(unquoted), nil
 }
 
 func (boolevator *Boolevator) Eval(expr string, env map[string]string) (bool, error) {
-	// Work around cases when the expression is terminated with \n, e.g.:
-	//
-	// only_if: >
-	//   $CIRRUS_PR != '' ||
-	//   $CIRRUS_BRANCH == 'master' ||
-	//   $CIRRUS_BRANCH =~ 'release/.*'
-	expr = strings.TrimSpace(expr)
-
-	// Work around text/scanner stopping at newline when scanning strings
-	expr = strings.ReplaceAll(expr, "\n", "\\n")
-
 	// Ensure that we keep the env as is
 	localEnv := make(map[string]string)
 	for key, value := range env {
