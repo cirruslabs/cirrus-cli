e756614867ea3dca0125d47f8e6cfc9705234656
fedor <fedor@cirruslabs.org>
2021-03-25 12:28:10 -0400

    Refactored tests

--- internal/commands/internal/test/test_test.go
+++ internal/commands/internal/test/test_test.go
@@ -14,9 +14,32 @@ import (
 	"testing"
 )
 
+// Makes sure all the test cases can be executed successfully
+func TestAll(t *testing.T) {
+	fileInfos, err := ioutil.ReadDir("testdata")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, fileInfo := range fileInfos {
+		fileInfo := fileInfo
+		t.Run(fileInfo.Name(), func(t *testing.T) {
+			runTestCommandAndGetOutput(t, filepath.Join("testdata", fileInfo.Name()))
+		})
+	}
+}
+
 // TestSimple ensures that a simple test is discovered and ran successfully.
 func TestSimple(t *testing.T) {
-	testutil.TempChdirPopulatedWith(t, "testdata/simple")
+	output := runTestCommandAndGetOutput(t, "testdata/simple")
+
+	adaptedPath := filepath.FromSlash("dir/subdir")
+	assert.Contains(t, output, fmt.Sprintf("'%s' succeeded", adaptedPath))
+}
+
+// Verify tests succeed and return console output
+func runTestCommandAndGetOutput(t *testing.T, sourceDir string) string {
+	testutil.TempChdirPopulatedWith(t, sourceDir)
 
 	// Create os.Stderr writer that duplicates it's output to buf
 	buf := bytes.NewBufferString("")
@@ -30,26 +53,5 @@ func TestSimple(t *testing.T) {
 
 	require.Nil(t, err)
 
-	adaptedPath := filepath.FromSlash("dir/subdir")
-	assert.Contains(t, buf.String(), fmt.Sprintf("'%s' succeeded", adaptedPath))
-}
-
-func TestDryRunAll(t *testing.T) {
-	fileInfos, err := ioutil.ReadDir("testdata")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for _, fileInfo := range fileInfos {
-		fileInfo := fileInfo
-		t.Run(fileInfo.Name(), func(t *testing.T) {
-			testutil.TempChdirPopulatedWith(t, filepath.Join("testdata", fileInfo.Name()))
-
-			command := commands.NewRootCmd()
-			command.SetArgs([]string{"internal", "test", "-o simple"})
-			err := command.Execute()
-
-			require.Nil(t, err)
-		})
-	}
+	return buf.String()
 }
