bf1b12b268a9b032ce5098e0cb8ed8af97a598d6
Nikolay Edigaryev <edigaryev@gmail.com>
2021-03-26 15:59:35 +0300

    Persistent worker: don't downgrade agent's version (#351)

--- internal/executor/instance/runconfig/runconfig.go
+++ internal/executor/instance/runconfig/runconfig.go
@@ -5,6 +5,7 @@ import (
 	"github.com/cirruslabs/cirrus-cli/internal/executor/options"
 	"github.com/cirruslabs/cirrus-cli/internal/executor/platform"
 	"github.com/cirruslabs/echelon"
+	"github.com/hashicorp/go-version"
 )
 
 type RunConfig struct {
@@ -30,4 +31,24 @@ func (rc *RunConfig) GetAgentVersion() string {
 
 func (rc *RunConfig) SetAgentVersion(agentVersion string) {
 	rc.agentVersion = agentVersion
+}
+
+func (rc *RunConfig) SetAgentVersionWithoutDowngrade(agentVersion string) error {
+	if agentVersion == "" {
+		return nil
+	}
+
+	requestedVersion, err := version.NewVersion(agentVersion)
+	if err != nil {
+		return err
+	}
+	defaultVersion := version.Must(version.NewVersion(platform.DefaultAgentVersion))
+
+	if requestedVersion.LessThan(defaultVersion) {
+		rc.agentVersion = defaultVersion.String()
+	} else {
+		rc.agentVersion = requestedVersion.String()
+	}
+
+	return nil
 }
---
+++ internal/executor/instance/runconfig/runconfig_test.go
@@ -0,0 +1,28 @@
+package runconfig_test
+
+import (
+	"fmt"
+	"github.com/cirruslabs/cirrus-cli/internal/executor/instance/runconfig"
+	"github.com/cirruslabs/cirrus-cli/internal/executor/platform"
+	"github.com/stretchr/testify/assert"
+	"math"
+	"testing"
+)
+
+func TestSetAgentVersionWithoutDowngrade(t *testing.T) {
+	rc := &runconfig.RunConfig{}
+
+	// No downgrade
+	prettyLowVersion := "0.1.0"
+	if err := rc.SetAgentVersionWithoutDowngrade(prettyLowVersion); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, platform.DefaultAgentVersion, rc.GetAgentVersion())
+
+	// Only upgrade
+	prettyHighVersion := fmt.Sprintf("%d.0.0", math.MaxInt32)
+	if err := rc.SetAgentVersionWithoutDowngrade(prettyHighVersion); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, prettyHighVersion, rc.GetAgentVersion())
+}
