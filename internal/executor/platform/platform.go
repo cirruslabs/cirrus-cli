09515b167fc2046b9f9d10f8170768c6dd2b97ff
Nikolay Edigaryev <edigaryev@gmail.com>
2022-02-22 20:41:21 +0300

    Bump agent to 1.73.2

--- internal/executor/platform/platform.go
+++ internal/executor/platform/platform.go
@@ -11,7 +11,7 @@ const (
 	agentImageBase = "gcr.io/cirrus-ci-community/cirrus-ci-agent:v"
 
 	// DefaultAgentVersion represents the default version of the https://github.com/cirruslabs/cirrus-ci-agent to use.
-	DefaultAgentVersion = "1.45.0"
+	DefaultAgentVersion = "1.73.2"
 )
 
 type CopyCommand struct {
