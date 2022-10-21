df7e13a9bb19683881f5a892a3ae9f23f7910dcd
Fedor Korotkov <fedor@cirruslabs.org>
2022-10-21 17:00:18 -0400

    Update all dependencies (#575)
    * Update dependencies
    
    * Fixed lint issue
    
    * Updated default agent version as well

--- internal/executor/platform/platform.go
+++ internal/executor/platform/platform.go
@@ -11,7 +11,7 @@ const (
 	agentImageBase = "gcr.io/cirrus-ci-community/cirrus-ci-agent:v"
 
 	// DefaultAgentVersion represents the default version of the https://github.com/cirruslabs/cirrus-ci-agent to use.
-	DefaultAgentVersion = "1.85.0"
+	DefaultAgentVersion = "1.91.0"
 )
 
 type CopyCommand struct {
