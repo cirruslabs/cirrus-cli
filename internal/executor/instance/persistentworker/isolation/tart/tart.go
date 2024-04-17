a7fb83348cd1c14282053ab2f0f920526ed988ef
Nikolay Edigaryev <edigaryev@gmail.com>
2024-04-17 17:56:21 +0400

    Tart: mount the working directory to $HOME when --dirty is specified (#723)
    * Respect --{,tart-}lazy-pull for non-standby Tart instances
    
    * Tart: mount the working directory to $HOME when --dirty is specified
    
    To avoid issues with spaces in "/Volumes/My Shared Files".

--- internal/executor/instance/persistentworker/isolation/tart/tart.go
+++ internal/executor/instance/persistentworker/isolation/tart/tart.go
@@ -33,7 +33,7 @@ var (
 
 const (
 	vmNamePrefix                = "cirrus-cli-"
-	macOSAutomountDirectoryPath = "/Volumes/My Shared Files"
+	macOSAutomountDirectoryPath = "$HOME/working-dir"
 	macOSAutomountDirectoryItem = "working-dir"
 )
 
@@ -50,7 +50,9 @@ type Tart struct {
 	display     string
 	volumes     []*api.Isolation_Tart_Volume
 
-	vm *VM
+	vm              *VM
+	initializeHooks []remoteagent.WaitForAgentHook
+	terminateHooks  []remoteagent.WaitForAgentHook
 }
 
 func New(
@@ -101,7 +103,7 @@ func (tart *Tart) Warmup(
 	additionalEnvironment map[string]string,
 	logger *echelon.Logger,
 ) error {
-	return tart.bootVM(ctx, ident, additionalEnvironment, "", logger)
+	return tart.bootVM(ctx, ident, additionalEnvironment, "", false, logger)
 }
 
 func (tart *Tart) bootVM(
@@ -109,6 +111,7 @@ func (tart *Tart) bootVM(
 	ident string,
 	additionalEnvironment map[string]string,
 	automountDir string,
+	lazyPull bool,
 	logger *echelon.Logger,
 ) error {
 	ctx, prepareInstanceSpan := tracer.Start(ctx, "prepare-instance")
@@ -128,7 +131,7 @@ func (tart *Tart) bootVM(
 	tmpVMName := vmNamePrefix + identToBeInjected + uuid.NewString()
 	vm, err := NewVMClonedFrom(ctx,
 		tart.vmName, tmpVMName,
-		false, // always clone from the base image
+		lazyPull,
 		additionalEnvironment,
 		logger,
 	)
@@ -143,11 +146,17 @@ func (tart *Tart) bootVM(
 
 	var directoryMounts []directoryMount
 	if automountDir != "" {
+		tag := fmt.Sprintf("tart.virtiofs.%s", macOSAutomountDirectoryItem)
+
 		directoryMounts = append(directoryMounts, directoryMount{
 			Name:     macOSAutomountDirectoryItem,
 			Path:     automountDir,
+			Tag:      tag,
 			ReadOnly: false,
 		})
+
+		tart.initializeHooks = append(tart.initializeHooks, mountWorkingDirectoryHook(tag, logger))
+		tart.terminateHooks = append(tart.terminateHooks, unmountWorkingDirectoryHook(logger))
 	}
 
 	// Convert volumes to directory mounts
@@ -237,7 +246,7 @@ func (tart *Tart) Run(ctx context.Context, config *runconfig.RunConfig) (err err
 			automountProjectDir = config.ProjectDir
 		}
 		err = tart.bootVM(ctx, strconv.FormatInt(config.TaskID, 10), config.AdditionalEnvironment,
-			automountProjectDir, config.Logger())
+			automountProjectDir, config.TartOptions.LazyPull, config.Logger())
 		if err != nil {
 			return err
 		}
@@ -249,8 +258,8 @@ func (tart *Tart) Run(ctx context.Context, config *runconfig.RunConfig) (err err
 		return err
 	}
 
-	initializeHooks := tart.initializeHooks(config)
-	terminateHooks := tart.terminateHooks(config)
+	initializeHooks := tart.getInitializeHooks(config)
+	terminateHooks := tart.getTerminateHooks(config)
 
 	addTartListBreadcrumb(ctx)
 	addDHCPDLeasesBreadcrumb(ctx)
@@ -333,7 +342,7 @@ func Cleanup() error {
 	return nil
 }
 
-func (tart *Tart) initializeHooks(config *runconfig.RunConfig) []remoteagent.WaitForAgentHook {
+func (tart *Tart) getInitializeHooks(config *runconfig.RunConfig) []remoteagent.WaitForAgentHook {
 	var hooks []remoteagent.WaitForAgentHook
 
 	if config.ProjectDir != "" && !config.DirtyMode {
@@ -365,12 +374,14 @@ func (tart *Tart) initializeHooks(config *runconfig.RunConfig) []remoteagent.Wai
 
 				sshSess, err := sshClient.NewSession()
 				if err != nil {
+					syncLogger.Finish(false)
 					return err
 				}
 
 				if err := sshSess.Run(command); err != nil {
 					_ = sshSess.Close()
 
+					syncLogger.Finish(false)
 					return err
 				}
 
@@ -382,10 +393,10 @@ func (tart *Tart) initializeHooks(config *runconfig.RunConfig) []remoteagent.Wai
 		})
 	}
 
-	return hooks
+	return append(tart.initializeHooks, hooks...)
 }
 
-func (tart *Tart) terminateHooks(config *runconfig.RunConfig) []remoteagent.WaitForAgentHook {
+func (tart *Tart) getTerminateHooks(config *runconfig.RunConfig) []remoteagent.WaitForAgentHook {
 	var hooks []remoteagent.WaitForAgentHook
 
 	targetfulVolumes := lo.Filter(tart.volumes, func(volume *api.Isolation_Tart_Volume, index int) bool {
@@ -420,7 +431,7 @@ func (tart *Tart) terminateHooks(config *runconfig.RunConfig) []remoteagent.Wait
 		})
 	}
 
-	return hooks
+	return append(tart.terminateHooks, hooks...)
 }
 
 func addTartListBreadcrumb(ctx context.Context) {
