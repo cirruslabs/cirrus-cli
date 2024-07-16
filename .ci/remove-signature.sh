37e55dd8194e0cf37adbb2e9fc0008da67c98952
Nikolay Edigaryev <edigaryev@gmail.com>
2024-07-16 11:50:56 +0400

    .goreleaser.yml: remove binary signatures before notarizing (#754)
    * .goreleaser.yml: remove binary signatures before notarizing
    
    * .goreleaser.yml: ignore "codesign" errors

---
+++ .ci/remove-signature.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set shell options to enable fail-fast behavior
+#
+# * -e: fail the script when an error occurs or command fails
+# * -u: fail the script when attempting to reference unset parameters
+# * -o pipefail: by default an exit status of a pipeline is that of its
+#                last command, this fails the pipe early if an error in
+#                any of its commands occurs
+#
+set -euo pipefail
+
+codesign --remove-signature "$@" || true
