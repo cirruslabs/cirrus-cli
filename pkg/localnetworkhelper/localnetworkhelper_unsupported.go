d8c9cc93db03147228c6a247ba771894e1fa73a0
Nikolay Edigaryev <edigaryev@gmail.com>
2024-11-26 17:33:37 +0100

    Add missing SSHClient stub in unsupported localnetworkhelper

--- pkg/localnetworkhelper/localnetworkhelper_unsupported.go
+++ pkg/localnetworkhelper/localnetworkhelper_unsupported.go
@@ -5,7 +5,10 @@ package localnetworkhelper
 import (
 	"context"
 	"fmt"
+	"golang.org/x/crypto/ssh"
 )
+
+var SSHClient *ssh.Client
 
 func StartAndConnect(ctx context.Context) error {
 	return fmt.Errorf("macOS \"Local Network\" helper is not supported on this platform")
