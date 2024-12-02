3c6903ff8c576bd690084eaaa4087d7f84ecbc3b
Nikolay Edigaryev <edigaryev@gmail.com>
2024-12-02 21:05:35 +0100

    pwdir: don't forget to os.Chown() in case privelege dropping is used

--- internal/executor/instance/persistentworker/pwdir/pwdir.go
+++ internal/executor/instance/persistentworker/pwdir/pwdir.go
@@ -1,6 +1,7 @@
 package pwdir
 
 import (
+	"github.com/cirruslabs/cirrus-cli/pkg/privdrop"
 	"os"
 	"path/filepath"
 )
@@ -12,5 +13,26 @@ func StaticTempDirWithDynamicFallback() (string, error) {
 		return staticTempDir, nil
 	}
 
-	return os.MkdirTemp("", "cirrus-build-")
+	// Make sure that the agent binary belongs to the privilege-dropped
+	// user and group, in case privilege dropping was requested
+	if chownTo := privdrop.ChownTo; chownTo != nil {
+		if err := os.Chown(staticTempDir, chownTo.UID, chownTo.GID); err != nil {
+			return "", err
+		}
+	}
+
+	tempDir, err := os.MkdirTemp("", "cirrus-build-")
+	if err != nil {
+		return "", err
+	}
+
+	// Make sure that the agent binary belongs to the privilege-dropped
+	// user and group, in case privilege dropping was requested
+	if chownTo := privdrop.ChownTo; chownTo != nil {
+		if err := os.Chown(tempDir, chownTo.UID, chownTo.GID); err != nil {
+			return "", err
+		}
+	}
+
+	return tempDir, nil
 }
