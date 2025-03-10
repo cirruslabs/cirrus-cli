8975b43e5ce7eab155c03bbb62fe4ba2b330f5e9
fedor <fedor.korotkov@gmail.com>
2025-03-10 10:20:51 +0400

    Log blob proxying progress
    Every minute in case proxying is taking longer than one minute.
    
    Hope it will help with understanding the issues with R2 when downloads timeout.

--- internal/agent/http_cache/azureblob/getblob.go
+++ internal/agent/http_cache/azureblob/getblob.go
@@ -5,11 +5,13 @@ import (
 	"github.com/cirruslabs/cirrus-cli/internal/agent/client"
 	"github.com/cirruslabs/cirrus-cli/pkg/api"
 	"io"
+	"log"
 	"net/http"
 	"time"
 )
 
 const PROXY_DOWNLOAD_BUFFER_SIZE = 1024 * 1024
+const PROXY_DOWNLOAD_PROGRESS_LOG_INTERVAL = 60 * time.Second
 
 func (azureBlob *AzureBlob) getBlobAbstract(writer http.ResponseWriter, request *http.Request) {
 	switch request.URL.Query().Get("comp") {
@@ -76,14 +78,33 @@ func (azureBlob *AzureBlob) getBlob(writer http.ResponseWriter, request *http.Re
 	}
 
 	startProxyingAt := time.Now()
+	progressLoggedAt := startProxyingAt
 	// we usually proxy large files so let's use a larger buffer
 	largeBuffer := make([]byte, PROXY_DOWNLOAD_BUFFER_SIZE)
-	bytesRead, err := io.CopyBuffer(writer, resp.Body, largeBuffer)
-	if err != nil {
-		proxyingDuration := time.Since(startProxyingAt)
-		fail(writer, request, http.StatusInternalServerError, "failed to proxy cache entry download",
-			"err", err, "duration", proxyingDuration, "read", bytesRead, "key", key)
-
-		return
+	bytesRead := int64(0)
+	for {
+		n, proxyErr := resp.Body.Read(largeBuffer)
+		if n > 0 {
+			if _, writeErr := writer.Write(largeBuffer[:n]); writeErr != nil {
+				proxyErr = writeErr
+				break
+			}
+			bytesRead += int64(n)
+		}
+		if proxyErr == io.EOF {
+			proxyErr = nil
+			break
+		}
+		if proxyErr != nil {
+			proxyingDuration := time.Since(startProxyingAt)
+			fail(writer, request, http.StatusInternalServerError, "failed to proxy cache entry download",
+				"err", proxyErr, "duration", proxyingDuration, "read", bytesRead, "key", key)
+			break
+		}
+		if time.Since(progressLoggedAt) > PROXY_DOWNLOAD_PROGRESS_LOG_INTERVAL {
+			currentSpeed := float64(bytesRead) / 1024 / 1024 / time.Since(startProxyingAt).Seconds()
+			log.Printf("Proxying cache entry download for %s: %d bytes read in %s (avg speed %.2f Mb/s)\n", key, bytesRead, time.Since(startProxyingAt), currentSpeed)
+			progressLoggedAt = time.Now()
+		}
 	}
 }
