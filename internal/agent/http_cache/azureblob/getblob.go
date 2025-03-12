53c456446911c4e3cad330f7f459611c0a2f4de4
fedor <fedor.korotkov@gmail.com>
2025-03-12 22:56:36 +0400

    GHA cache: just 307 to a pre-singed URL

--- internal/agent/http_cache/azureblob/getblob.go
+++ internal/agent/http_cache/azureblob/getblob.go
@@ -3,11 +3,7 @@ package azureblob
 import (
 	"fmt"
 	"github.com/cirruslabs/cirrus-cli/internal/agent/client"
-	"github.com/cirruslabs/cirrus-cli/internal/agent/progressreader"
 	"github.com/cirruslabs/cirrus-cli/pkg/api"
-	"github.com/dustin/go-humanize"
-	"io"
-	"log"
 	"net/http"
 	"time"
 )
@@ -47,52 +43,5 @@ func (azureBlob *AzureBlob) getBlob(writer http.ResponseWriter, request *http.Re
 		return
 	}
 
-	// Proxy cache entry download
-	req, err := http.NewRequestWithContext(request.Context(), http.MethodGet,
-		generateCacheDownloadURLResponse.Urls[0], nil)
-	if err != nil {
-		fail(writer, request, http.StatusInternalServerError, "failed to create request to proxy"+
-			" cache entry download", "key", key, "err", err)
-
-		return
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		fail(writer, request, http.StatusInternalServerError, "failed to perform request to proxy"+
-			" cache entry download", "key", key, "err", err)
-
-		return
-	}
-
-	switch resp.StatusCode {
-	case http.StatusOK:
-		// proceed with proxying
-	case http.StatusNotFound:
-		writer.WriteHeader(http.StatusNotFound)
-
-		return
-	default:
-		fail(writer, request, http.StatusInternalServerError, fmt.Sprintf("failed to perform request to proxy"+
-			" cache entry download, got unexpected HTTP %d", resp.StatusCode), "key", key)
-
-		return
-	}
-
-	startProxyingAt := time.Now()
-	// we usually proxy large files so let's use a larger buffer
-	largeBuffer := make([]byte, PROXY_DOWNLOAD_BUFFER_SIZE)
-	progressReader := progressreader.New(resp.Body, PROXY_DOWNLOAD_PROGRESS_LOG_INTERVAL, func(bytes int64, duration time.Duration) {
-		rate := float64(bytes) / duration.Seconds()
-
-		log.Printf("Proxying cache entry download for %s: %d bytes read in %s (%s/s)",
-			key, bytes, duration, humanize.Bytes(uint64(rate)))
-	})
-	bytesRead, err := io.CopyBuffer(writer, progressReader, largeBuffer)
-	if err != nil {
-		proxyingDuration := time.Since(startProxyingAt)
-		fail(writer, request, http.StatusInternalServerError, "failed to proxy cache entry download",
-			"err", err, "duration", proxyingDuration, "read", bytesRead, "key", key)
-		return
-	}
+	http.Redirect(writer, request, generateCacheDownloadURLResponse.Urls[0], http.StatusTemporaryRedirect)
 }
