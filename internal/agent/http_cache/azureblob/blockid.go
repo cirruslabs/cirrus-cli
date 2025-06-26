b19efefb9ce03939331f1bf49b2d80986c7af553
Nikolay Edigaryev <edigaryev@gmail.com>
2025-06-27 00:55:56 +0200

    http_cache(azureblob): support Azure SDK for Golang-generated block IDs

--- internal/agent/http_cache/azureblob/blockid.go
+++ internal/agent/http_cache/azureblob/blockid.go
@@ -2,35 +2,63 @@ package azureblob
 
 import (
 	"encoding/base64"
+	"encoding/binary"
 	"fmt"
 	"strconv"
 )
 
-// Constant from Azure SDK for JS[1]
-//
-// [1]: https://github.com/Azure/azure-sdk-for-js/blob/fc4cbf0e10e15cbbe7cf873294db7d6e2bd02851/sdk/storage/storage-blob/src/utils/utils.common.ts#L486-L487
-const maxBlockIndexLength = 6
+const (
+	// Azure SDK for JS limits its block size to 48 bytes[1]
+	//
+	// [1]: https://github.com/Azure/azure-sdk-for-js/blob/fc4cbf0e10e15cbbe7cf873294db7d6e2bd02851/sdk/storage/storage-blob/src/utils/utils.common.ts#L483-L484
+	jsMaxSourceStringLength = 48
 
-func blockIDToPartNumber(blockIDRaw string) (int, error) {
+	// Constant from Azure SDK for JS[1]
+	//
+	// [1]: https://github.com/Azure/azure-sdk-for-js/blob/fc4cbf0e10e15cbbe7cf873294db7d6e2bd02851/sdk/storage/storage-blob/src/utils/utils.common.ts#L486-L487
+	jsMaxBlockIndexLength = 6
+
+	// Azure SDK for Golang uses fixed 64-byte blocks[1]
+	//
+	// [1]: https://github.com/Azure/azure-sdk-for-go/blob/42f1cc3136bf4f0a7d9a36d634576bbf8521beef/sdk/storage/azblob/blockblob/chunkwriting.go#L227
+	golangFixedBlockLength = 64
+)
+
+func blockIDToPartNumber(blockIDRaw string) (uint32, error) {
 	// Decode the Base64-encoded block ID
 	blockIDBytes, err := base64.StdEncoding.DecodeString(blockIDRaw)
 	if err != nil {
 		return 0, err
 	}
 
-	// Extract the index from the block ID
-	blockID := string(blockIDBytes)
+	var blockIndex uint32
 
-	if len(blockID) < maxBlockIndexLength {
-		return 0, fmt.Errorf("block ID is too small to contain the index number")
-	}
+	switch len(blockIDBytes) {
+	case jsMaxSourceStringLength:
+		// Extract the index from the block ID
+		blockID := string(blockIDBytes)
 
-	rawBlockIndex := blockID[len(blockID)-maxBlockIndexLength:]
+		if len(blockID) < jsMaxBlockIndexLength {
+			return 0, fmt.Errorf("block ID is too small to contain the index number")
+		}
 
-	// Parse the index as an integer
-	blockIndex, err := strconv.Atoi(rawBlockIndex)
-	if err != nil {
-		return 0, err
+		rawBlockIndex := blockID[len(blockID)-jsMaxBlockIndexLength:]
+
+		// Parse the index as an integer
+		preBlockIndex, err := strconv.ParseUint(rawBlockIndex, 10, 32)
+		if err != nil {
+			return 0, err
+		}
+
+		blockIndex = uint32(preBlockIndex)
+	case golangFixedBlockLength:
+		// Reverse of what Azure SDK for Golang does[1]
+		//
+		// [1]: https://github.com/Azure/azure-sdk-for-go/blob/42f1cc3136bf4f0a7d9a36d634576bbf8521beef/sdk/storage/azblob/blockblob/chunkwriting.go#L243
+		blockIndex = binary.BigEndian.Uint32(blockIDBytes[16:])
+	default:
+		return 0, fmt.Errorf("unknown block ID format: expected block ID of size %d or %d bytes, got %d bytes",
+			jsMaxSourceStringLength, golangFixedBlockLength, len(blockIDBytes))
 	}
 
 	// Part numbers start with 1
--- internal/agent/http_cache/azureblob/blockid_test.go
+++ internal/agent/http_cache/azureblob/blockid_test.go
@@ -5,8 +5,20 @@ import (
 	"testing"
 )
 
-func TestBlockIDToPartNumber(t *testing.T) {
+func TestBlockIDToPartNumberJS(t *testing.T) {
+	// Azure SDK for JS format
 	partNumber, err := blockIDToPartNumber("Yzg4ODM0YjYtZmI3MC00OWNmLWJlYmEtNDliODFjNDE0MWM3MDAwMDAwMDAwMDAx")
 	require.NoError(t, err)
-	require.Equal(t, 2, partNumber)
+	require.EqualValues(t, 2, partNumber)
+}
+
+func TestBlockIDToPartNumberGolang(t *testing.T) {
+	// Azure SDK for Golang format
+	partNumber, err := blockIDToPartNumber("6eiRh4oEQZ1cibBlHr1wFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==")
+	require.NoError(t, err)
+	require.EqualValues(t, 1, partNumber)
+
+	partNumber, err = blockIDToPartNumber("6eiRh4oEQZ1cibBlHr1wFAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==")
+	require.NoError(t, err)
+	require.EqualValues(t, 2, partNumber)
 }
