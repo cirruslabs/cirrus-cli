c5db6224bc7a52bd4d641ce06dac0817dfd1a238
Fedor Korotkov <fedor.korotkov@gmail.com>
2025-08-21 17:52:09 -0400

    Use `automatic` caching for Tart VMs
    Seems in most of the cases it actually yields better results than forcing caching.

--- internal/executor/instance/persistentworker/isolation/tart/vm.go
+++ internal/executor/instance/persistentworker/isolation/tart/vm.go
@@ -189,7 +189,7 @@ func (vm *VM) Start(
 	go func() {
 		defer vm.wg.Done()
 
-		args := []string{"--no-graphics", "--no-clipboard", "--no-audio", "--root-disk-opts", "caching=cached,sync=none"}
+		args := []string{"--no-graphics", "--no-clipboard", "--no-audio", "--root-disk-opts", "caching=automatic,sync=none"}
 
 		if softnet {
 			args = append(args, "--net-softnet")
