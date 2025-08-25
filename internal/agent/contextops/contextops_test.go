12f234fc3960f90aaa82503db161745ef00bf8ac
Nikolay Edigaryev <edigaryev@gmail.com>
2025-08-26 00:00:18 +0200

    Test both ways

--- internal/agent/contextops/contextops_test.go
+++ internal/agent/contextops/contextops_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestAnd(t *testing.T) {
+func TestAndLeftToRight(t *testing.T) {
 	firstCtx, firstCtxCancel := context.WithCancel(context.Background())
 	firstCtxCancel()
 
@@ -18,6 +18,20 @@ func TestAnd(t *testing.T) {
 
 	waitStart := time.Now()
 	<-contextops.And(firstCtx, secondCtx).Done()
+	waitStop := time.Now()
+
+	require.GreaterOrEqual(t, waitStop.Sub(waitStart), 15*time.Second)
+}
+
+func TestAndRightToLeft(t *testing.T) {
+	firstCtx, firstCtxCancel := context.WithCancel(context.Background())
+	firstCtxCancel()
+
+	secondCtx, secondCtxCancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer secondCtxCancel()
+
+	waitStart := time.Now()
+	<-contextops.And(secondCtx, firstCtx).Done()
 	waitStop := time.Now()
 
 	require.GreaterOrEqual(t, waitStop.Sub(waitStart), 15*time.Second)
