752603e8e7c6295326b9a2caea363e6aa287191b
Nikolay Edigaryev <edigaryev@gmail.com>
2025-10-27 19:48:28 +0400

    Evaluator: enable httpcache logging when debug logging is enabled (#960)

--- internal/commands/root.go
+++ internal/commands/root.go
@@ -1,14 +1,19 @@
 package commands
 
 import (
+	"log/slog"
+
 	"github.com/cirruslabs/cirrus-cli/internal/commands/helpers"
 	"github.com/cirruslabs/cirrus-cli/internal/commands/internal"
 	"github.com/cirruslabs/cirrus-cli/internal/commands/localnetworkhelper"
 	"github.com/cirruslabs/cirrus-cli/internal/commands/validate"
 	"github.com/cirruslabs/cirrus-cli/internal/commands/worker"
+	"github.com/cirruslabs/cirrus-cli/internal/logginglevel"
 	"github.com/cirruslabs/cirrus-cli/internal/version"
 	"github.com/spf13/cobra"
 )
+
+var debug bool
 
 func NewRootCmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -17,7 +22,16 @@ func NewRootCmd() *cobra.Command {
 		Version:       version.FullVersion,
 		SilenceUsage:  true,
 		SilenceErrors: true,
+		PersistentPreRunE: func(_ *cobra.Command, _ []string) error {
+			if debug {
+				logginglevel.Level.Set(slog.LevelDebug)
+			}
+
+			return nil
+		},
 	}
+
+	cmd.PersistentFlags().BoolVar(&debug, "debug", false, "enable debug logging")
 
 	commands := []*cobra.Command{
 		validate.NewValidateCmd(),
