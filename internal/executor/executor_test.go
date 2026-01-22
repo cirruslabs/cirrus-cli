4063df8268212332ee0083eb354984d91260eb11
Fedor Korotkov <fedor.korotkov@gmail.com>
2026-01-22 12:29:44 +0100

    Bump agent to 0.161.1
    Add cache socket test

--- internal/executor/executor_test.go
+++ internal/executor/executor_test.go
@@ -225,6 +225,16 @@ func TestCache(t *testing.T) {
 	assert.NoError(t, err)
 }
 
+func TestCacheSocket(t *testing.T) {
+	if _, ok := os.LookupEnv("CIRRUS_CONTAINER_BACKEND"); !ok {
+		t.Skip("no container backend configured")
+	}
+
+	dir := testutil.TempDirPopulatedWith(t, "testdata/cache-socket")
+	err := testutil.Execute(t, dir)
+	assert.NoError(t, err)
+}
+
 func TestCacheOptimisticRestore(t *testing.T) {
 	if _, ok := os.LookupEnv("CIRRUS_CONTAINER_BACKEND"); !ok {
 		t.Skip("no container backend configured")
